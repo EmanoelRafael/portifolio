c733eff5595db3aa54a417cdc8b466155154b999
EmanoelRafael <emanoel.rafael@ufpe.br>
2024-09-03 16:15:14 -0300

    "Adding the Hero componente - 3D animation"

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
     <nav
       className= {`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
     >
-      <div className='w-full flex justify-between items-center max-w-7x1 mx-auto'>
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
         <Link 
           to="/"
           className='flex items-center gap-2'
@@ -22,8 +22,8 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}>
             <img src={logo} alt="logo" className='w-9 h-9 object-contain'/>
-            <p className='text-white text-[18px] font-bold cursor-pointer flex'>Adrian &nbsp;
-              <span className='sm:block hidden'> | Javascript Mastery</span>
+            <p className='text-white text-[18px] font-bold cursor-pointer flex'>ERMFS &nbsp;
+              <span className='sm:block hidden'> | Computer Engineering</span>
             </p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
