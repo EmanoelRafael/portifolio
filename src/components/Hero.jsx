341ad706e763a00874ffb5e2623f03176d059c80
EmanoelRafael <emanoel.rafael@ufpe.br>
2024-09-05 15:41:39 -0300

    "removing ComputersCanvas"

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,29 +2,34 @@ import { motion } from 'framer-motion';
 
 import { styles } from '../styles';
 import { ComputersCanvas } from './canvas';
+import { logo } from '../assets';
 
 const Hero = () => {
   return (
     <section className='relative w-full h-screen mx-auto' 
     >
-      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-col items-start gap-y-80`}
       >
-        <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#915eff]'/>
-          <div className='w-1 sm:h-80 h-40 violet-gradient'/>
+        <div className='flex flex-row gap-5'>
+          <div className='flex flex-col justify-center items-center mt-5'>
+            <div className='w-5 h-5 rounded-full bg-[#915eff]'/>
+            <div className='w-1 sm:h-80 h-40 violet-gradient'/>
+          </div>
+
+          <div>
+            <h1 className={`${styles.heroHeadText} text-white`}>
+              Hi, I'm <span className='text-[#915eff]'>Emanoel</span>
+            </h1>
+            <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+              I'm a computer engineering student <br className='sm:block hidden'/>
+              and I solve problems with technology.
+            </p>
+          </div>
         </div>
 
-        <div >
-          <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915eff]'>Emanoel</span>
-          </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I'm a computer engineering student <br className='sm:block hidden'/>
-            and I solve problems with technology.
-          </p>
-        </div>
-
-         {/* <ComputersCanvas />  */}
+         {/* <div className='border-gray-50 border-2 bottom-50 w-full h-fit flex justify-center'>
+          <img src={logo} alt="logo" className='sm:h-[700px] h-[200px] ' />
+         </div> */}
 
         <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
           <a href="#about">
