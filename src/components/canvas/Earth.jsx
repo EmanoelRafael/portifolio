1da2d8d2d2062e23813838bd844292f144241ca4
EmanoelRafael <emanoel.rafael@ufpe.br>
2024-09-07 20:17:20 -0300

    "solving the gltf bug"

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -5,7 +5,7 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 import CanvasLoader from '../Loader'
 
 const Earth = () => {
-  const earth = useGLTF('./planet/scene.gltf')
+  const earth = useGLTF('./portifolio/src/assets/planet/scene.gltf')
 
   return (
     <primitive 
@@ -36,6 +36,8 @@ const EarthCanvas = () => {
             minPolarAngle={Math.PI/2}/>
 
           <Earth />
+
+          <Preload all />
         </Suspense>
     </Canvas>
   )
