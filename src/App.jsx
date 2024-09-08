b31201a23cc1f4cb7fefcd624c4c12184fbd49db
EmanoelRafael <emanoel.rafael@ufpe.br>
2024-09-07 23:02:43 -0300

    'adding starsCanva as background'

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
 import { BrowserRouter } from "react-router-dom"
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
+import { About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <div className="relative z-0">
           <Navbar />
           <Hero />
-        </div>
-        <About />
-        <Experience />
-        <Tech />
-        <Works />
-        {/* <Feedbacks /> */}
-        <div className="relative z-0">
+          <About />
+          <Experience />
+          <Tech />
+          <Works />
           <Contact />
           <StarsCanvas />
         </div>
